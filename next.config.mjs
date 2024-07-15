4f9d4710c27ac9d25b51ae97527324d3bb25d940
Faezeh Roohbakhsh <froohbakhsh@gmail.com>
2024-07-15 15:54:34 +0330

    PWA settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,26 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import withPWA from "@ducanh2912/next-pwa";
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+};
+
+const pwaConfig = {
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swcMinify: true,
+  dest: "public",
+  fallbacks: {
+    //image: "/static/images/fallback.png",
+    document: "/offline", // Path to the offline page
+    // font: '/static/font/fallback.woff2',
+    // audio: ...,
+    // video: ...,
+  },
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+  // ... other options you like
+};
+
+export default withPWA(pwaConfig)(nextConfig);
---
+++ src/app/offline/page.tsx
@@ -0,0 +1,9 @@
+// pages/offline.js
+export default function Offline() {
+    return (
+      <div>
+        <h1>You are offline</h1>
+        <p>Please check your internet connection and try again.</p>
+      </div>
+    );
+  }
